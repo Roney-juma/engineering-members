d6f78fe57e21385a7c0333eafb552263f73033cc
Roney <roney@NDK36-roney-mac.local>
2024-10-10 11:02:03 +0300

    initial Commit

---
+++ sacco_project/members/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Member(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    membership_number = models.CharField(max_length=50, unique=True)
+    join_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+
