3066e5ab9ff8e5c0752427347e1ad7dd0f775cfd
Roney <roney@NDK36-roney-mac.local>
2024-10-10 21:18:59 +0300

    Admin Endpoin ts

--- sacco_project/members/admin.py
+++ sacco_project/members/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member, Project
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'username', 'phone_number', 'membership_number', 'join_date')
+    search_fields = ('full_name', 'email', 'username', 'phone_number', 'membership_number')
+    list_filter = ('join_date',)
+    readonly_fields = ('join_date',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('start_date', 'end_date')
+    readonly_fields = ('created_at', 'updated_at')
