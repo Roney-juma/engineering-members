81f51046ad39634bd0f6b4abbb0b5e22592cd1f1
Roney <roney@NDK36-roney-mac.local>
2024-10-12 10:54:22 +0300

    CORSs

--- sacco_project/sacco_project/settings.py
+++ sacco_project/sacco_project/settings.py
@@ -59,11 +59,6 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://mesk-website.vercel.app",
-
-]
 CORS_ALLOW_HEADERS = [
     'accept',
     'authorization',
